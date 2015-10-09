0b749ff1d98858a11ef5b211b750cff33b9e5938
Hellblazer <hellblazer@me.com>
2015-10-09 10:03:39 -0700

    serve explorer out of root.  map phantasm api to /api/*

--- src/main/resources/ui/js/CustomerMasterDetailCtrl.js
+++ src/main/resources/ui/js/CustomerMasterDetailCtrl.js
@@ -56,7 +56,7 @@ myApp
         '$scope',
         'Restangular',
         function ($scope, Restangular) {
-            var Northwind = Restangular.one('/graphql').one('workspace').all(encodeURIComponent(northwindUri));
+            var Northwind = Restangular.one('/api/graphql').one('workspace').all(encodeURIComponent(northwindUri));
             var ordersQuery = 'query customers($id: String!) { Customer( id: $id) { orders {id name requiredDate orderDate shipDate itemDetails { id name unitPrice quantity product { id name } } } } }';
             var instancesQuery = '{ InstancesOfCustomer { customerName id description } }';
 
